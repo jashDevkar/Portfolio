04b0d156a3be6dfea7bd160fd8c773665b957667
jashDevkar <devkarjash1054@gmail.com>
2024-10-14 23:54:11 +0530

    Added hover on cards

---
+++ src/components/Title.jsx
@@ -0,0 +1,29 @@
+import { FaArrowRight } from "react-icons/fa";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+
+function Title() {
+
+    const [text]=useTypewriter({
+        words:['Web developer','App developer'],
+        loop:{},
+      })
+  return (
+    <section className=" flex justify-center items-center h-[90vh] flex-col text-center border-b-gray-500 border-b-2 mt-[20px]">
+          <img src="dp.jpg" className="w-[200px] h-[200px] rounded-full object-cover"/>  
+          <h1 className="font-Roboto font-semibold text-[16px] md:text-[28px] w-[40%] text-center mt-[10px]"><span className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 text-transparent bg-clip-text">I'm Jash Devkar </span> {text}<Cursor cursorColor="pink"/> </h1>
+        
+          <div className="md:w-[40%] flex justify-evenly mt-[20px] gap-3 md:gap-0  ">
+            <button className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 rounded-e-full rounded-s-full pt-2 pb-2 pl-3 pr-3 text-[12px] md:text-[16px] ">Connect with me</button>
+            <button className="rounded-e-full rounded-s-full pt-2 pb-2 pl-3 pr-3 border-gray-400 border-2 text-[12px] md:text-[16px]">My resume</button>
+            <button className="flex justify-center items-center gap-1 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 rounded-e-full rounded-s-full pt-2 pb-2 pl-3 pr-3 text-[12px] md:text-[16px]">About me <FaArrowRight/></button>
+          </div>
+
+          {/* <div className=" bg-slate-900 w-[300px] h-full md:absolute right-[10px]  ">
+
+          </div> */}
+        </section>
+  )
+}
+
+export default Title
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -4,23 +4,26 @@ function Skills() {
 
 
     const myCards=[
-        {title:'React js',imagePath:'Reactjs.png'},
-        {title:'React Native',imagePath:'Reactjs.png'},
-        {title:'Django',imagePath:'python.jpg'},
-        {title:'Flutter',imagePath:'flutter.png'},
-        {title:'Node js',imagePath:'Nodejs.png'}
+        {title:'React js',imagePath:'Reactjs.png',url:'https://react.dev/learn'},
+        {title:'React Native',imagePath:'Reactjs.png',url:'https://reactnative.dev/docs/environment-setup'},
+        {title:'Django',imagePath:'python.jpg',url:'https://docs.djangoproject.com/en/5.1/'},
+        {title:'Flutter',imagePath:'flutter.png',url:'https://flutter.dev/learn'},
+        {title:'Node js',imagePath:'Nodejs.png',url:'https://nodejs.org/en'}
     ]
 
   return (
     <section >
-        <header className="h-[10vh] text-center text-4xl mt-2 font-semibold">Skills</header>
+        <header className="h-[10vh] text-center text-4xl mt-4 font-semibold">Skills</header>
 
         <div className="flex flex-wrap gap-10 mb-2 justify-center items-center md:px-7">
 
             {
                 myCards.map((item,index)=>(
-                    <div key={index} className="md:w-[220px] w-[300px] h-[250px] bg-slate-900 rounded-lg cursor-pointer hover:shadow-lg hover:shadow-slate-400 transition-shadow" >
-                        <img src={item.imagePath} className="md:w-[220px] w-[300px] h-[210px] rounded-lg"/>
+                    <div key={index} className="md:w-[220px] w-[230px] h-[250px] bg-[#2C2C2C] rounded-lg cursor-pointer hover:shadow-lg hover:shadow-[#D054A0] transition-shadow relative group shadow-sm" >
+                        <div className="absolute bg-black/40 group-hover:h-full h-[80%] flex justify-center items-center opacity-0 group-hover:opacity-100 group-hover:bottom-0 group-hover:w-full transition-all duration-300">
+                            <button className="p-2 hidden group-hover:block transition-all duration-500  rounded-lg bg-gradient-to-r  from-purple-500 via-pink-500 to-orange-500  text-white"><a href={item.url}>Learn topic</a></button>
+                        </div>
+                        <img src={item.imagePath} className="md:w-[220px] w-[230px] h-[210px] rounded-lg"/>
                         <h1 className="m-2 font-bold font-Roboto ">{item.title}</h1>
                     </div>
                 ))
