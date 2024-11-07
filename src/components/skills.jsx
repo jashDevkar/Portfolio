5b018ce18647b0b4cd1e2d5cbad4304591fd4241
jashDevkar <devkarjash1054@gmail.com>
2024-11-08 04:37:48 +0530

    test 2

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,7 +7,7 @@ const Skills = ({ skillRef }) => {
     reverse: false, // reverse the tilt direction
     max: 40, // max tilt rotation (degrees)
     perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 2, // 2 = 200%, 1.5 = 150%, etc..
+    scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
     speed: 1000, // Speed of the enter/exit transition
     transition: true, // Set a transition on enter/exit.
     axis: null, // What axis should be disabled. Can be X or Y.
