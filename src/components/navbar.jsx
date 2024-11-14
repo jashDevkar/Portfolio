25676222a19545e43120c50fe221b1fbb25b1c4d
jashDevkar <devkarjash1054@gmail.com>
2024-11-14 08:11:35 +0530

    added transition in navbaar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,21 +13,30 @@ function Navbar({data}) {
    setOpen(!open)
   }
 
+  const openTransition=()=>{
+    if(open){
+      return  'right-2'
+    }
+    else{
+      return 'right-0'
+    }
+  }
+
   return (
-    <nav className="flex md:justify-around justify-between px-11 w-full  border-b-gray-500 border-b-2 py-3  items-center shadow-md bg-[#161512]/50  backdrop-blur-sm">
+    <nav className={`flex md:justify-around justify-between px-11 w-full  border-b-gray-500 border-b-2 py-3  items-center shadow-md bg-[#161512]/50  backdrop-blur-sm relative `}>
       <h1 className="font-Pacifico text-[32px]  bg-gradient-to-r  from-purple-500 via-pink-500 to-orange-500 text-transparent bg-clip-text cursor-pointer"><a>Jash</a></h1>
       {
         open?
         null:
         <div className="md:hidden "><IoMenu size={32} onClick={toggleOpen}/></div>
       }
-        <ul className={` hidden md:static md:flex flex-row h-fit justify-evenly md:w-[40%] md:bg-transparent text-white font-semibold tracking-wide `}>
+        <ul className={` hidden md:static md:flex flex-row h-fit justify-evenly md:w-[40%] md:bg-transparent text-white font-semibold tracking-wide overflow-hidden`}>
           <div className={`md:hidden ${open?'flex justify-end ':'hidden'}`} onClick={toggleOpen}><IoClose size={20}/></div>
           <li><button onClick={data[0]} className="nav-link relative">Skills</button></li>
           <li><button onClick={data[1]} className="nav-link relative">Project</button></li>
           <li><button onClick={data[3]} className="nav-link relative">Connect me</button></li>
         </ul>
-        <ul className={`md:hidden w-[40%] mr-5 pl-4 pr-2 text-white  ${open?"absolute right-0 mr-2 top-2":"hidden"} pt-2 pb-4 mt-2 rounded bg-gradient-to-br from-[#0e0c0b] via-[#221f1d] to-[#2d2a28] backdrop-blur-xl`}>
+        <ul className={`top-2 pl-4 pb-4 pr-2 pt-2 w-[50%] md:hidden ${open?'right-2 absolute ':'right-[-2clsl00px] absolute '} rounded bg-gradient-to-br from-[#0e0c0b] via-[#221f1d] to-[#2d2a28] backdrop-blur-xl transition-all ease-in-out duration-1000`}>
           <li className="flex w-[100%] justify-end"><IoClose size={24} onClick={toggleOpen}/></li>
           <li className="" onClick={data[0]}>Skills</li>
           <li className="mt-2" onClick={data[1]}>Projects</li>
