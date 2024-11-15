dd12a057c8f1e55670c939e855708ea9843303d5
jashDevkar <devkarjash1054@gmail.com>
2024-11-15 12:38:51 +0530

    solved bug in navbaar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,7 +54,7 @@ function Navbar({ data }) {
       </button>
 
       <div
-        className={`top-2 gap-7 flex flex-col pl-4 pb-4 pr-2 pt-2 w-[50%] h-[29vh] z-20 md:hidden ${
+        className={`top-2 gap-3 flex flex-col pl-4 pb-4 pr-2 pt-2 w-[50%] h-fit z-20 md:hidden ${
           open ? "right-2 absolute" : "right-[-200px] absolute"
         } rounded bg-gradient-to-br from-[#0e0c0b] via-[#221f1d]/95 to-[#2d2a28]/90 backdrop-blur-xl transition-all ease-in-out duration-1000`}
       >
